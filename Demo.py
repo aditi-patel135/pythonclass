d26ad850fa41ee94e169263f9eba898546f12507
aditi-patel135 <aditipatel2xx531@gmail.com>
2026-03-12 19:27:44 +0530

    Add files via upload

---
+++ Demo.py
@@ -0,0 +1,128 @@
+# math = 40
+# pi = 3.14
+# name = 'aditi'
+# print(type(math))
+# print(type(pi))
+# print(type(name))
+# print("2"+"2")
+# val1 = input("enter 1 num")
+# val2 = input("enter 2 num")
+# print(val1+val2)
+# print(int(3.14))
+# print(int(True))
+# print(int(False))
+# print(int("4"))
+# print(float(3.14))
+# print(float(True))
+# print(float(False))
+# print(float("4"))
+# print(complex(3.14))
+# print(complex(True))
+# print(complex(False))
+# print(complex("4"))
+# print(complex(3))
+# print(complex(12.5))
+# # print(complex("4.6"))
+# print(bool(3.14))
+# print(bool(0))
+# print(bool(15))
+# print(bool(False))
+# print(bool(-1))
+# print(bool(0+0))
+# print(bool(0+2))
+
+# P = 10000
+# R = 10
+# T = 15
+# SI = P*R*T/100
+# print(SI)
+# c = 37
+# f = c*1.8 +32
+# print(f)
+ 
+#swapping 2 vals usning trmp
+# val1 = 10
+# val2 = 20
+# temp = val1
+# val1 = val2
+# val2 = temp
+# print(val1)
+# print(val2)
+#swapping eithout thirt vasriabke
+# a = 10
+# b = 20
+# a = a+b
+# b = a-b
+# a  = a-b
+# print(a)
+# print(b)
+             
+# enter height in feet and convert it to inch and cm
+# height =float (input("enter your heignt in feets"))
+# feet = height*12
+# cm = height* 30.48
+# print(feet)
+# print(cm)
+# num = 1
+# for i in range(7):
+#     print(i--)
+
+# accpet 3 paper marks and find maximum using nested if else
+# n1=int(input("enter value of n1"))
+# n2=int(input("enter value of n2"))
+# n3=int(input("enter value of n3"))
+# if n1>n2:
+#     iif pe
+#     print()
+mydict = {
+    "name": "prashant",
+    "professional": "developer",
+    "empid":1001
+}
+# print(mydict)
+# print(type(mydict))
+
+# mydict = {
+#     101:"prashant",🤧🤧🤧🤧
+#     102:"prashant",
+#     "103":"trivensh",
+#     "104":"anisih",
+#     101:"anish",
+
+# }
+# print(mydict)
+
+# for x in mydict.values():
+#     print(x)
+# mydict.pop()
+# name = "abcd"
+# print(name[0])
+# print(name[1])
+# print(name[-1])
+# print(name[0:5])
+# print(name[1:])
+# print(name[:5])
+# print(name[:])
+# print(name[1:8:2])
+# print(name[::-1])
+# s = "blah blahh blahhh blahhhh"
+# print(s.find("blah"))
+# print(s.find("blahhh"))
+# print(s.find("blahhhh"))
+# print(s.find("blahh"))
+
+# print('subjectwise:')
+# phy = 50
+# m = 60
+# c = 40
+# print("physics")
+# print
+
+# a = 50 
+# b = 60
+# c = 
+
+name = "prashant"
+data = ['a','e','i','o','u']
+vowels =0
+con  = 0
