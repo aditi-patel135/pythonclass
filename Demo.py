510c7d26ff69c4771176117cbdc872dc6e915f21
aditi-patel135 <aditipatel2xx531@gmail.com>
2026-03-16 11:57:14 +0530

    d5Demo.py

--- Demo.py
+++ Demo.py
@@ -122,8 +122,76 @@ mydict = {
 # b = 60
 # c = 
 
-name = "prashant"
-data = ['a','e','i','o','u']
-vowels =0
+# name = "prashant"
+# data = ['a','e','i','o','u']
+# vowels =0
 
-con  = 0
+# con  = 0
+n=int(input("enter num of rows"))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#          print((n+1-i),end ="")
+#     print()
+# 
+# def add():
+#     n1 = int(input("enter the value of n1"))
+#     n2 = int(input("enter the value of n2"))
+#     print("add=:", n1+n2)
+
+# add()
+#function
+# def msg(): #called function
+#     print("hello patel")
+#     n1=int(input("enter no"))
+#     n2=int(input("enter no"))
+#     print("ADD=",n1+n1)
+# msg()
+# 2 type fuct 1. pre defined 2. user defined 
+
+#how to return multiple value
+# def calculation():
+#     n1=int(input("enter no"))
+#     n2=int(input("enter no"))
+#     sum=n1+n2
+#     mul=n1*n2
+#     div=n1/n2
+#     sub=n1-n2
+#     return sum ,mul ,div, sub
+# result=calculation()
+# print(result)
+
+# py pass 4 types of argument we can pass
+#1.positional argument
+#2. keyword   3.default   4. variable length argument / variable number of argument 
+
+
+# def personalInfo(fname,lname):
+#     print("first name ",fname)
+#     print("last name ",lname)
+# personalInfo("khizra","raien")    
+
+#1.positonal 
+# def personalInfo(fname,lname):
+#     print("first name ",fname)
+#     print("last name ",lname)
+# fname="khizra"
+# lname="raien"
+# personalInfo("fname","lname")
+
+#2. default argument
+# def cityName(city="delhi"):
+#     print(city)
+# cityName("mumbai")
+# cityName("Nagpur")
+# cityName()
+
+# #variable length argument
+# def studentName(*name):
+#     print(name)
+# studentName("khizra","aditi","patel")
+# mylist = [5,2,9,7,5,6]
+# def searchElement(target):
+#  for i in range(len(mylist)):
+#    if target == mylist[i]:
+#      print("elemrny at index " , i)
+# searchElement(7)   
